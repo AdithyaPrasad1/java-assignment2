5bc6aae929adfdd1d839f8e82b1a242d4c98f0b6
AdithyaPrasad <hadithya71@gmail.com>
2023-06-09 21:49:30 +0530

    first commit

---
+++ squre.js
@@ -0,0 +1,5 @@
+var a=[1,3,9,12,15,18,21];
+for(var i=0;i<7;i++){
+    sq=a[i]**2
+    console.log(sq)
+}
---
+++ table 8.js
@@ -0,0 +1,5 @@
+let n=8,i=1;
+for (let i=1;i<=10;i++){
+    result=i*n
+    console.log(i,"x",n,"=",result); 
+}
